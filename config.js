8d807628910afd176dbdf0a760ee8ebb6be100e2
Jin Igarashi <igarashi.jin@water-gis.com>
2023-04-12 16:37:22 +0900

    Initial commit

---
+++ config.js
@@ -0,0 +1,113 @@
+require('dotenv').config();
+
+module.exports = {
+    db: {
+      user:process.env.db_user,
+      password:process.env.db_password,
+      host:process.env.db_host,
+      port:process.env.db_port,
+      database:process.env.db_name,
+    },
+    name: 'narwassco_vectortiles',
+    description: 'Vector tiles for water & sanitation data in Narok water, Kenya',
+    attribution: '©Narok Water and Sewerage Services Co., Ltd.',
+    mbtiles: __dirname + '/data/data.mbtiles',
+    ghpages: __dirname + '/public/tiles',
+    createPmtiles: true,
+    minzoom: 10,
+    maxzoom: 16,
+    layers : [
+        {
+            name: 'pipeline',
+            geojsonFileName: __dirname + '/pipeline.geojson',
+            select: `
+            SELECT row_to_json(featurecollection) AS json FROM (
+                SELECT
+                  'FeatureCollection' AS type,
+                  array_to_json(array_agg(feature)) AS features
+                FROM (
+                  SELECT
+                    'Feature' AS type,
+                    ST_AsGeoJSON(ST_TRANSFORM(ST_MakeValid(x.geom),4326))::json AS geometry,
+                    row_to_json((
+                      SELECT t FROM (
+                        SELECT
+                          16 as maxzoom,
+                          10 as minzoom
+                      ) AS t
+                    )) AS tippecanoe,
+                    row_to_json((
+                      SELECT p FROM (
+                        SELECT
+                          x.pipeid as fid,
+                          a.name as pipetype,
+                          x.pipesize,
+                          b.name as material,
+                          x.constructiondate,
+                          x.insertdate,
+                          x.updatedate,
+                          x."Town"
+                      ) AS p
+                    )) AS properties
+                  FROM pipenet x
+                  INNER JOIN pipetype a
+                  ON x.pipetypeid = a.pipetypeid
+                  INNER JOIN material b
+                  ON x.materialid = b.materialid
+                  WHERE NOT ST_IsEmpty(x.geom)
+                ) AS feature
+              ) AS featurecollection
+            `
+        },
+        {
+          name: 'meter',
+          geojsonFileName: __dirname + '/meter.geojson',
+          select:`
+          SELECT row_to_json(featurecollection) AS json FROM (
+            SELECT
+              'FeatureCollection' AS type,
+              array_to_json(array_agg(feature)) AS features
+            FROM (
+              SELECT
+              'Feature' AS type,
+              ST_AsGeoJSON(ST_TRANSFORM(x.geom,4326))::json AS geometry,
+              row_to_json((
+                SELECT t FROM (
+                  SELECT
+                    16 as maxzoom,
+                    16 as minzoom
+                ) AS t
+              )) AS tippecanoe,
+              row_to_json((
+                SELECT p FROM (
+                  SELECT
+                  x.meterid as fid,
+                  a.name as metertype,
+                  x.pipesize as diameter,
+                  x.zonecd,
+                  CASE WHEN x.connno=-1 THEN NULL ELSE LPAD(CAST(x.connno as text), 4, '0') END as connno,
+                  x.installationdate,
+                  b.status,
+                  x.serialno,
+                  b.name as customer,
+                  c.name as village,
+                  x.insertdate,
+                  x.updatedate,
+                  x.isgrantprj as isjica
+                ) AS p
+              )) AS properties
+              FROM meter x
+              INNER JOIN metertype a
+              ON x.metertypeid = a.metertypeid
+              LEFT JOIN customer b
+              ON x.zonecd = b.zonecd
+              AND x.connno = b.connno
+              LEFT JOIN village c
+			        on b.villageid = c.villageid
+              WHERE NOT ST_IsEmpty(x.geom) AND x.metertypeid = 1
+            ) AS feature
+          ) AS featurecollection
+          `
+        }
+    ],
+};
